d6d909398a7f9eafca893545fdd3e3ecd50845b1
Raymond Ndlovu <ndlovuraymond@MacBook-Pro-Raymond.local>
2023-05-21 21:19:30 +0200

    Added time series data for Lebrons scoring prediction for the next 5 years

--- Lebron_Time_Series.py
+++ Lebron_Time_Series.py
@@ -11,31 +11,33 @@ data = pd.read_csv("Lebron_data.csv")
 data["Date"] = pd.to_datetime(data["Date"])
 data.set_index("Date", inplace=True)
 
+yearly_ppg = data.groupby(data.index.year).mean()
 # Split the data into training and testing sets
-train_data = data.iloc[
-    -400:-164
+train_data = yearly_ppg.iloc[
+    :-2
 ]  # Use data from 2015 onwards except the last 2 seasons for training
-test_data = data.iloc[-164:]  # Last 2 years for testing
+test_data = yearly_ppg.iloc[-2:]  # Last 2 years for testing
 
 # Fit the ARIMA model
-model = ARIMA(train_data["pts"], order=(50, 1, 1))  # Example order, adjust as needed
+model = ARIMA(train_data["pts"], order=(11, 1, 1))  # Example order, adjust as needed
 model_fit = model.fit()
 
 # Forecast 2 years ahead
-forecast = model_fit.forecast(steps=164)  # Forecasting 164 games which is (2 years)
-df = forecast.to_frame(name=None)
+forecast = model_fit.forecast(steps=5)  # Forecasting 82 games which is (1 season)
+df = forecast.to_frame(name="pts")
 
-# Create a new DataFrame for the forecasted values
-forecast_dates = pd.date_range(start=test_data.index[163], periods=164, freq="5D")
-forecast_df = pd.DataFrame(
-    df["predicted_mean"], index=forecast_dates, columns=["forecasted_value"]
-)
+# Creating yearly forecast thing for the next year
+forecast_dates = pd.Series([2016,2017,2018,2019,2020])
+forecast_pts = pd.Series(forecast.array,index=range(0,5))
+forecast_df = pd.concat([forecast_pts.rename("pts"),forecast_dates.rename("Date")],axis=1)
+forecast_df.set_index("Date",inplace=True)
 
 # Plot the actual and forecasted values
-plt.plot(data.index, data["pts"], label="Actual")
-plt.plot(forecast_df.index, df["predicted_mean"], label="Forecast")
-plt.xlabel("Date")
-plt.ylabel("Value")
-plt.title("Time Series Forecast")
+plt.plot(yearly_ppg.index, yearly_ppg["pts"], label="Actual")
+plt.plot(forecast_df.index, forecast_df["pts"], label="Forecast")
+plt.xlabel("Year")
+plt.ylabel("Points Per Game")
+plt.title("Time Series Forecast For Lebron's Scoring")
+plt.xticks([2005,2010,2015,2020],["2005","2010","2015","2020"])
 plt.legend()
 plt.show()
